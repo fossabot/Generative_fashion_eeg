860477916130261bf5a7dd368fbd9783860ea43f
blackSwan566 <fionamariele@gmail.com>
2025-05-14 15:18:01 +0200

    running flusk & react app

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -15,5 +15,15 @@ def members():
         ]
     } 
     
+@app.route("/ratings", methods=["POST"] )
+def ratings():
+    new_rating=request.json
+    ratings=[]
+    ratings.append(new_rating)
+    print(ratings)
+    return jsonify({"message": "Member added successfully", "member": new_rating})
+
+     
+    
 if __name__ == "__main__":  
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
